9eee7b9d1fa15cf5dca916241db32f8ac0e65df6
Ariadna <ariadna.999@yandex.ru>
2026-01-27 15:11:02 +0300

    first version of the website is done

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const mediaImage = document.querySelectorAll('.media-grid-box-image');
+
+mediaImage.forEach(
+  media => {
+    media.addEventListener('click', function() {
+      media.classList.toggle('active');
+    });
+  }
+);
+
+const burgerButton = document.querySelector('.header-burger-button');
+
+const burgerMenu = document.querySelector('.header-menu');
+
+burgerButton.addEventListener('click', function() {
+  burgerButton.classList.toggle('active-button');
+  burgerMenu.classList.toggle('active-menu');
+});
