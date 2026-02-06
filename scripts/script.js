21fc02746afb68704155147145e71b34afe2cfa4
Ariadna <ariadna.999@yandex.ru>
2026-02-06 10:08:09 +0300

    decoration changes added and several node files

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,16 @@
 const mediaImage = document.querySelectorAll('.media-grid-box-image');
 
-mediaImage.forEach(
-  media => {
-    media.addEventListener('click', function() {
-      media.classList.toggle('active');
-    });
-  }
-);
+mediaImage.forEach((media) => {
+  media.addEventListener('click', function () {
+    media.classList.toggle('active');
+  });
+});
 
 const burgerButton = document.querySelector('.header-burger-button');
 
 const burgerMenu = document.querySelector('.header-menu');
 
-burgerButton.addEventListener('click', function() {
+burgerButton.addEventListener('click', function () {
   burgerButton.classList.toggle('active-button');
   burgerMenu.classList.toggle('active-menu');
 });
